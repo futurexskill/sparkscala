09896a55c484b9ba0523e1059a5e8bf6e119d14b
futureXskill <54407776+futurexskill@users.noreply.github.com>
2020-09-02 08:17:18 -0700

    Add files via upload

--- FutureXSparkScalaProject/src/test/scala/common/FutureXBase.scala
+++ FutureXSparkScalaProject/src/test/scala/common/FutureXBase.scala
@@ -1,7 +1,8 @@
 package common
 
 import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
 
-abstract class FutureXBase extends AnyFlatSpec {
+abstract class FutureXBase extends AnyFlatSpec with Matchers  {
 
 }
--- FutureXSparkScalaProject/src/test/scala/common/SparkTransformerSpec.scala
+++ FutureXSparkScalaProject/src/test/scala/common/SparkTransformerSpec.scala
@@ -46,4 +46,40 @@ class SparkTransformerSpec extends FutureXBase {
       val transformedDF = SparkTraformer.replaceNullValues(df)
     }
   }
-}
+  it should "also replace null value with unknown" in {
+    val df : DataFrame = spark.read
+      .option("header", "true")
+      .option("inferSchema","true")
+      .csv("mock_course_data.csv")
+    df.show()
+    val transformedDF = SparkTraformer.replaceNullValues(df)
+    transformedDF.show()
+    val authors = transformedDF
+      .filter(transformedDF("course_id")
+        .equalTo("2"))
+      .select("author_name")
+      .collectAsList()
+
+    assertResult("Unknown") {
+      authors.get(0)(0)
+    }
+
+  }
+  it should "2  also replace null value with unknown" in {
+    val df : DataFrame = spark.read
+      .option("header", "true")
+      .option("inferSchema","true")
+      .csv("mock_course_data.csv")
+    df.show()
+    val transformedDF = SparkTraformer.replaceNullValues(df)
+    transformedDF.show()
+    val authors = transformedDF
+      .filter(transformedDF("course_id")
+        .equalTo("2"))
+      .select("author_name")
+      .collectAsList()
+
+     "Unknown" should equal (authors.get(0)(0))
+  }
+
+  }
